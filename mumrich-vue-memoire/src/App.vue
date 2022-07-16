72dd6a88409c6f164090c0faa95b0241ba691804
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-07-16 14:18:49 +0200

    remove some default content.

--- mumrich-vue-memoire/src/App.vue
+++ mumrich-vue-memoire/src/App.vue
@@ -6,12 +6,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -22,9 +18,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
