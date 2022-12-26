a8acb0e624ded20e8b083c6ef55fb9b43380678e
Hannes Morgenthaler <hanneszbinden@outlook.com>
2022-12-26 14:49:31 +0100

    use basic vuetify wireframe with mdi icons.

--- vue-lien/src/App.vue
+++ vue-lien/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { RouterView } from "vue-router";
+
+const drawer = ref<boolean | null>(null);
 </script>
 
-<template>
+<!-- <template>
   <header>App</header>
   <nav>
     <RouterLink to="/">Home</RouterLink>
@@ -10,4 +13,22 @@ import { RouterView } from "vue-router";
   </nav>
 
   <RouterView />
+</template> -->
+
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
