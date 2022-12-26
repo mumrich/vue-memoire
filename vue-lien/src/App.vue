3ca710bf5731d06ca70aa5ec448fe221a0f6d3ae
Hannes Morgenthaler <hanneszbinden@outlook.com>
2022-12-26 15:34:51 +0100

    setup pwa.

--- vue-lien/src/App.vue
+++ vue-lien/src/App.vue
@@ -5,30 +5,20 @@ import { RouterView } from "vue-router";
 const drawer = ref<boolean | null>(null);
 </script>
 
-<!-- <template>
-  <header>App</header>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
-
-  <RouterView />
-</template> -->
-
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
-      <!--  -->
+      <v-list>
+        <v-list-item to="/" title="Home" />
+        <v-list-item to="/about" title="About" />
+      </v-list>
     </v-navigation-drawer>
-
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
-
     <v-main>
-      <!--  -->
+      <RouterView />
     </v-main>
   </v-app>
 </template>
--- vue-lien/vite.config.ts
+++ vue-lien/vite.config.ts
@@ -3,10 +3,43 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "logo-192x192.png", "logo-512x312.png"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
