e3a51981cfb686db31f5e5225e5e286368dc30a4
Hannes Morgenthaler <hanneszbinden@outlook.com>
2022-12-26 15:58:20 +0100

    cleanup form-view.

--- vue-lien/src/views/FormView.vue
+++ vue-lien/src/views/FormView.vue
@@ -6,34 +6,23 @@
       :rules="nameRules"
       label="Name"
       required
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-
+    />
+    <v-text-field v-model="email" :rules="emailRules" label="E-mail" required />
     <v-select
       v-model="select"
       :items="items"
       :rules="[(v) => !!v || 'Item is required']"
       label="Item"
       required
-    ></v-select>
-
+    />
     <v-checkbox
       v-model="checkbox"
       :rules="[(v) => !!v || 'You must agree to continue!']"
       label="Do you agree?"
       required
-    ></v-checkbox>
-
+    />
     <v-btn color="success" class="mr-4" @click="validate"> Validate </v-btn>
-
     <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-
     <v-btn color="warning" @click="resetValidation"> Reset Validation </v-btn>
   </v-form>
 </template>
@@ -43,19 +32,22 @@ import { ref } from "vue";
 
 const form = ref<HTMLFormElement>();
 const valid = ref(true);
+
 const name = ref("");
+const email = ref("");
+const select = ref(null);
+const checkbox = ref(false);
+
+const items = ref(["Item 1", "Item 2", "Item 3", "Item 4"]);
+
 const nameRules = [
   (v: any) => !!v || "Name is required",
   (v: any) => (v && v.length <= 10) || "Name must be less than 10 characters",
 ];
-const email = ref("");
 const emailRules = [
   (v: any) => !!v || "E-mail is required",
   (v: any) => /.+@.+\..+/.test(v) || "E-mail must be valid",
 ];
-const select = ref(null);
-const items = ref(["Item 1", "Item 2", "Item 3", "Item 4"]);
-const checkbox = ref(false);
 
 async function validate() {
   const { valid } = await form.value!.validate();
